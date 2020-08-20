48c8cfc5d1e4630c65d78c77163c24febcf894f2
Boaschti <sebastian-weisser@gmx.de>
2020-08-20 16:31:33 +0200

    Fix Bug if sensor is not Connected

--- sonoff/xsns_44_as608.ino
+++ sonoff/xsns_44_as608.ino
@@ -24,9 +24,6 @@ void as608init(){
 
     if ((pin[GPIO_AS608_RX] < 99) && (pin[GPIO_AS608_TX] < 99)){
 
-        as608selected = true;
-        //Adafruit_Fingerprint finger = Adafruit_Fingerprint(new TasmotaSerial(pin[GPIO_AS608_RX], pin[GPIO_AS608_TX], 0));
-
         serial = new (myTasmotaSerial) TasmotaSerial(pin[GPIO_AS608_RX], pin[GPIO_AS608_TX], 0);
         finger = new (myFingerprint) Adafruit_Fingerprint(serial, 0);
 
@@ -34,6 +31,7 @@ void as608init(){
 
         if (finger->verifyPassword()){
           finger->getTemplateCount();
+          as608selected = true;
           snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_LOG "AS60x found. %i fingerprints stored."), finger->templateCount);
         }else{
           snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_LOG "AS60x not found! Got data: %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X"), finger->mydata[0], finger->mydata[1], finger->mydata[2], finger->mydata[3], finger->mydata[4], finger->mydata[5], finger->mydata[6], finger->mydata[7], finger->mydata[8], finger->mydata[9], finger->mydata[10], finger->mydata[11]);
